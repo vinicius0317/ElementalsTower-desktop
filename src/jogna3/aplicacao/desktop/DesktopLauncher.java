41b7ac8ca5efd89cf0313e0869cc43152780472c
Vinicius Pinheiro <74792748+vinicius0317@users.noreply.github.com>
2021-12-09 00:33:41 -0300

    Add files via upload

--- src/jogna3/aplicacao/desktop/DesktopLauncher.java
+++ src/jogna3/aplicacao/desktop/DesktopLauncher.java
@@ -2,14 +2,18 @@ package jogna3.aplicacao.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import jogna3.aplicacao.ElementalsTower;
+import jogna3.aplicacao.helpers.GameInfos;
+import jogna3.aplicacao.helpers.GameManage;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "ElementalsTower";
-		config.width = 800;
-		config.height = 650;
+		config.title = "ElemenTower";
+		config.width = GameInfos.WIDTH;
+		config.height = GameInfos.HEIGHT;
+		config.foregroundFPS = GameManage.FPS;
 		new LwjglApplication(new ElementalsTower(), config);
 	}
 }
